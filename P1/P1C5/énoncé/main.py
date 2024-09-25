825e0bd47d04168bd9b521703cbde80a92b280bf
bonte2003 <bonte.nekianbe@ensea.edu.ci>
2024-09-25 02:18:45 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom='NEKIANBE'
+age=20
+taille=179.5
+est_etudiant=True
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
